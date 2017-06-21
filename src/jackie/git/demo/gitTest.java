21deab771afed386dee6b22efe417c9e93b2331e
guccio618 <jackiechan618@gmail.com>
2017-06-20 22:59:36 -0700

    [step 7]: in master branch

--- src/jackie/git/demo/gitTest.java
+++ src/jackie/git/demo/gitTest.java
@@ -4,6 +4,8 @@ public class gitTest {
 
 	public static void main(String[] args) {
 		System.out.println("[step 1]: in master branch");
-
+		
+		System.out.println("[step 7]: in master branch");
+		
 	}
 }
